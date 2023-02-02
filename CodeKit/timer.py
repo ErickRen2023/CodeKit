1cdd22526a07f13c373a22263b1326ed4000fd05
ErickRen <ErickRen2022@outlook.com>
2023-02-02 23:42:10 +0800

    docs: Add the timer doc.

--- CodeKit/timer.py
+++ CodeKit/timer.py
@@ -2,13 +2,16 @@ import time
 
 
 def timer(func):
+    """
+    Timer will calculate the run time of the decorated function and print run time in the Console.
+    :param func: decorated function
+    """
     def trigger(*args):
         start = time.time()
 
         result = func(*args)
 
         end = time.time()
-
         print("Run time -> %ss" % (end - start))
 
         return result
